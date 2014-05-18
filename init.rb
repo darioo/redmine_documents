ef710b2d3bc7d73ad1993a968f92cb7f418e6d73
darioo <sb.kowalwawa+github@gmail.com>
2014-05-18 08:48:10 +0200

    Add URL's

--- init.rb
+++ init.rb
@@ -3,7 +3,9 @@ require 'redmine'
 Redmine::Plugin.register :redmine_documents do
   name 'Redmine Comfortable Documents'
   author 'Kowalski Dariusz'
-  description 'This plugin improve of visualisation list of documents'
+  description 'This plugin improve of v  author_url 'https://github.com/darioo'
+  author_url 'https://github.com/darioo'
+  url "https://github.com/darioo/redmine_documents" if respond_to?(:url)
   version '1.0.0'
   requires_redmine :version_or_higher => '2.2.4'
 end
